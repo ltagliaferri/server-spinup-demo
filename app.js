aca951a5fd05ae4e19d8aed9cbd2563ad2092f26
Kat Juell <kathleenjuell@gmail.com>
2019-04-19 20:40:01 +0000

    created main landing page and single container compose file for first stage

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var express = require('express');
-var app = express();
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
 
-var path = __dirname;
+const path = __dirname;
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const port = process.env.PORT || 8080;
 
 router.use(function (req,res,next) {
   console.log('/' + req.method);
@@ -24,6 +23,6 @@ app.use(express.static(path));
 app.use('/posts', express.static('/posts/articles'));
 app.use('/', router);
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+  console.log(`Example app listening on ${port}!`)
 })
--- gulpfile.js
+++ gulpfile.js
@@ -28,9 +28,9 @@ gulp.task('vendor', function() {
     ])
     .pipe(gulp.dest('./vendor/bootstrap'))
 
-  // Font Awesome 5
+  // Font Awesome
   gulp.src([
-      './node_modules/@fortawesome/**/*'
+      './node_modules/@fortawesome/**/*',
     ])
     .pipe(gulp.dest('./vendor'))
 
@@ -40,12 +40,6 @@ gulp.task('vendor', function() {
       '!./node_modules/jquery/dist/core.js'
     ])
     .pipe(gulp.dest('./vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
 
 });
 
